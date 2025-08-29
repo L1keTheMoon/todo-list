55148d04c3beb8461ad5ebafcb207555c51daa31
Roman Burlakov <r.burlakov@mail.astondevs.ru>
2025-08-29 14:05:16 +0300

    initial commit

---
+++ src/test/setup.ts
@@ -0,0 +1,11 @@
+import { expect, afterEach } from 'vitest'
+import { cleanup } from '@testing-library/react'
+import * as matchers from '@testing-library/jest-dom/matchers'
+
+// Расширяем стандартные ожидания Vitest
+expect.extend(matchers)
+
+// Очищаем DOM после каждого теста
+afterEach(() => {
+  cleanup()
+})
